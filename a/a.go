2b545e62c80f9ac3776e81a2ed1b8251f35b09b3
myitcv <myitcv@example.com>
2018-11-07 16:18:37 +0000

    Add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/go-modules-by-example-staging/submodules/b"
+	"fmt"
+)
+
+const Name = b.Name
+
+func main() {
+	fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,3 @@
+module github.com/go-modules-by-example-staging/submodules/a
+
+require github.com/go-modules-by-example-staging/submodules/b v0.1.1
